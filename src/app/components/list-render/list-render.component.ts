095d0d083fed9fb7fbd8df3f6071eefe52f38e16
Verônica Paranhos <veronicahp@gmail.com>
2023-07-21 18:11:09 -0300

    subindo os arquivos do curso de Angular(2)

--- src/app/components/list-render/list-render.component.ts
+++ src/app/components/list-render/list-render.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { Animal } from 'src/app/Animal';
+import { ListService } from 'src/app/services/list.service';
 
 @Component({
   selector: 'app-list-render',
@@ -16,7 +17,14 @@ export class ListRenderComponent {
 
   animalDetails = '';
 
+  constructor(private listService: ListService) {}
+
   showAge(animal: Animal): void {
     this.animalDetails = `O pet ${animal.name} tem ${animal.age} anos`;
+  }
+
+  removeAnimal(animal: Animal): void {
+    console.log('Removendo animal...');
+    this.animals = this.listService.remove(this.animals, animal);
   }
 }
